e445044b8ef720fd4c274f0a338c2f9307b74bb7
Divya Sawant <86359301+DivyaRsawant@users.noreply.github.com>
2023-04-17 21:07:05 +0530

    Add files via upload

---
+++ FindFactors.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number :"))
+
+
+def factors(x):
+    print("The factors of", x, "are :")
+    for i in range(1, x + 1):
+        if x % i == 0:
+            print(i)
+
+
+factors(num)
